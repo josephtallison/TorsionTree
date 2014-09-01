1c8511169ad8fb26189bedcb14f51d9c00e7b733
Joseph Allison <jallison@Planck-JA.local>
2014-09-01 17:26:59 -0600

    Added doc strong for the Atom class

--- TTAtom.py
+++ TTAtom.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
 class Atom:
+    """Contains all information for each atom, including atom number (n),
+    xyz coordinates (x, y, z), the type of atom in both string and parameter
+    file index number (atomType *note this is a list*), the connectivity list
+    corresponding to index number within Python (connectivity), and whether or 
+    not the atom is to be rotated for the next torsional rotation.
+    """
     def __init__(self,n,x,y,z,atomType,connectivity):
         self.n = n
         self.x = x
